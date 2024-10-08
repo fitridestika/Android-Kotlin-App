4980ecc9e7104366e675d4be72ff8dc83ad32f15
SD-24 <fitridestika22@gmail.com>
2024-10-08 11:46:52 +0700

    First Commit

--- app/src/main/java/com/example/androidkotlinapp_desty/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinapp_desty/MainActivity.kt
@@ -2,53 +2,32 @@ package com.example.androidkotlinapp_desty
 
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.androidkotlinapp_desty.R.id.main
+import com.example.androidkotlinapp_desty.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    // Inisialisasi variabel counter
-    private var counter: Int = 0
+    private lateinit var binding: ActivityMainBinding
 
+    // Inisialisasi variabel counter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        // Set padding untuk system bars
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        // Set nilai awal tvCounter
-        val tvCounter = findViewById<TextView>(R.id.tvCounter)
-        tvCounter.text = counter.toString()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvCounter.text = "1"
     }
-
-    // Fungsi untuk menambah angka
-    fun addNumber(view: View) {
-        counter++
-        val tvCounter = findViewById<TextView>(R.id.tvCounter)
-        tvCounter.text = counter.toString()
+    fun addNumber(v: View){
+        val currVal = binding.tvCounter.text.toString().toInt()
+        val nextVal = currVal + 1
+        binding.tvCounter.text = nextVal.toString()
     }
-
-    // Fungsi untuk mengurangi angka
-    fun subtractNumber(view: View) {
-        counter--
-        val tvCounter = findViewById<TextView>(R.id.tvCounter)
-        tvCounter.text = counter.toString()
+    fun resetNumber(v: View){
+        binding.tvCounter.text = "0"
     }
-
-    // Fungsi untuk reset angka ke 0
-    fun resetNumber(view: View) {
-        counter = 0
-        val tvCounter = findViewById<TextView>(R.id.tvCounter)
-        tvCounter.text = counter.toString()
+    fun subtractNumber(v: View){
+        val currVal = binding.tvCounter.text.toString().toInt()
+        val nextVal = currVal - 1
+        binding.tvCounter.text = nextVal.toString()
     }
 }
